aed4d9fbe402e4c70cc21f914dd7563064a114ff
l1ttps <l1ttps443@gmail.com>
2025-09-09 08:17:31 +0000

    refactor(grpc-proxy): restructure gRPC client initialization and metadata handling

--- src/grpc-proxy/grpc-proxy.service.ts
+++ src/grpc-proxy/grpc-proxy.service.ts
@@ -1,35 +1,33 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
 import * as grpc from '@grpc/grpc-js';
-import { packageObjectHelper } from 'grpc-js-reflection-client';
-import { GrpcServiceMethod } from 'src/types/type';
-import { Request } from 'express';
 import { Metadata } from '@grpc/grpc-js';
-import { convertKeysToCamelCase } from 'src/helper/snakeToCamel';
+import { BadRequestException, Injectable, OnModuleInit } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { Request } from 'express';
+import { packageObjectHelper } from 'grpc-js-reflection-client';
+import { convertKeysToCamelCase } from 'src/helper/snakeToCamel';
+import { GrpcServiceMethod } from 'src/types/type';
 @Injectable()
-export class GrpcProxyService {
+export class GrpcProxyService implements OnModuleInit {
   constructor(private configService: ConfigService) {}
   private grpcReflectionInstance = {};
+  private grpcMetadata = new Metadata();
+  private grpcReflectionServer: string;
+  
+  onModuleInit() {
+    this.grpcReflectionServer = `0.0.0.0:${this.configService.get('PORT_GRPC', 50051)}`;
+  }
+  
   /**
-   * HTTP to gRPC bridge.
+   * Initializes the gRPC reflection instance for a given package and service.
+   * This method should be called during initialization to set up the gRPC client.
    *
-   * This method takes an Express `Request` and data as an argument, determines
-   * the gRPC package, service, and method to call, and calls the
-   * corresponding gRPC method. The method returns a Promise that resolves to
-   * the data returned by the gRPC method with all keys converted to camelCase.
-   *
-   * @param req An Express `Request` object.
-   * @param data The data to pass to the gRPC method.
-   * @returns A Promise that resolves to the data returned by the gRPC method.
-   * @throws A `BadRequestException` if the gRPC method throws an error.
+   * @param packageName The name of the gRPC package.
+   * @param service The name of the gRPC service.
    */
-  public async httpToGrpc(req: Request, data: any) {
-    const grpcMetadata = new Metadata();
-    const { packageName, service, method } = this.getServiceMethod(req);
-    const grpcReflectionServer = `0.0.0.0:${this.configService.get('PORT_GRPC', 50051)}`;
+  public async initGrpcClient(packageName: string, service: string) {
     const packageObject = await packageObjectHelper({
       credentials: grpc.ChannelCredentials.createInsecure(),
-      host: grpcReflectionServer,
+      host: this.grpcReflectionServer,
       proto_symbol: `${packageName}.${service}`,
       protoLoaderOptions: {
         keepCase: false,
@@ -46,13 +44,35 @@ export class GrpcProxyService {
     const [protoName, version] = packageName.split('.');
     this.grpcReflectionInstance[packageName] = new packageObject[protoName][
       version
-    ][service](grpcReflectionServer, grpc.ChannelCredentials.createInsecure());
+    ][service](this.grpcReflectionServer, grpc.ChannelCredentials.createInsecure());
+  }
+
+  /**
+   * HTTP to gRPC bridge.
+   *
+   * This method takes an Express `Request` and data as an argument, determines
+   * the gRPC package, service, and method to call, and calls the
+   * corresponding gRPC method. The method returns a Promise that resolves to
+   * the data returned by the gRPC method with all keys converted to camelCase.
+   *
+   * @param req An Express `Request` object.
+   * @param data The data to pass to the gRPC method.
+   * @returns A Promise that resolves to the data returned by the gRPC method.
+   * @throws A `BadRequestException` if the gRPC method throws an error.
+   */
+  public async httpToGrpc(req: Request, data: any) {
+    const { packageName, service, method } = this.getServiceMethod(req);
+    
+    // Initialize gRPC client if not already initialized
+    if (!this.grpcReflectionInstance[packageName]) {
+      await this.initGrpcClient(packageName, service);
+    }
 
     try {
       const result = await new Promise((resolve, reject) => {
         this.grpcReflectionInstance[packageName][method](
           data,
-          grpcMetadata,
+          this.grpcMetadata,
           (err, data) => {
             if (err) {
               reject(err);
